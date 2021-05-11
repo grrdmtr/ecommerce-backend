0ece55c107ed2de7477f8f9dd45012a866fb6a3c
grrdmtr <63411140+grrdmtr@users.noreply.github.com>
2021-05-11 21:21:59 +0300

    My first commit

---
+++ config/plugins.js
@@ -0,0 +1,22 @@
+module.exports = ({ env }) => {
+
+    if(env('NODE_ENV') === 'production') {
+        return{
+            upload: {
+                provider: 'aws-s3',
+                providerOptions: {
+                    accesKeyid: env('AWS_ACCESS_KEY_ID'),
+                    secretAccessKey: env('AWS_ACCESS_SECRET'),
+                    region: env('AWS_REGION'),
+                    params: {
+                        Bucket: env('AWS_BUCKET')
+                    }
+                }
+            }
+        }
+
+    }
+    return {
+        
+    }
+}
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd16545f0-16f8-4055-8dcd-94a5bae99cd2'
+};
