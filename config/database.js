3abd41d36f3d1434ca4a70ce103b308f19272893
grrdmtr <63411140+grrdmtr@users.noreply.github.com>
2021-05-18 20:46:01 +0300

    commit

--- config/database.js
+++ config/database.js
@@ -1,43 +1,25 @@
 const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 
-module.exports = ({ env }) => {
-  
-if(env('NODE_ENV') === 'production') {
-  const config = parse(process.env.DATABASE_URL)
-  return {
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'postgres',
-          host: config.host,
-          port: config.port,
-          database: config.database,
-          username: config.user,
-          password: config.password
-        },
-        options: {
-          ssl: false
-        }
-      }
-    }
-  }
-}
-
-  return {
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'sqlite',
-          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-        },
-        options: {
-          useNullAsDefault: true,
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
         },
       },
+      options: {
+        ssl: true,
+      },
     },
-  }
-};
+  },
+});
